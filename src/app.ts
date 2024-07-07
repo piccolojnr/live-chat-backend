481d57e4ecf58ec8d586c642054c9e9d9cebd392
AhmedEhab2022 <am8122004@gmail.com>
2024-07-07 16:03:35 +0300

    Edit app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import router from './routes';
 import { logger } from './utils/logger';
 import morgan from 'morgan';
+import mongoClient from './utils/db';
 
 dotenv.config();
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 
-mongoose.connect(process.env.MONGODB_URI!)
+mongoClient.connect(process.env.MONGODB_URI!)
     .then(() => logger.info('Connected to MongoDB'))
     .catch((err) => logger.error('Error connecting to MongoDB:', err));
 
