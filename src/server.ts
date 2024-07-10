b8062567a3db634ba72696c804b5f8d1200f9b4d
AhmedEhab2022 <am8122004@gmail.com>
2024-07-10 15:14:28 +0300

    Add basic socket.io events

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,57 @@
-import http from 'http';
-// import { Server } from 'socket.io';
-import app from './app';
-import { logger } from './utils/logger';
+import http from "http";
+import { Server } from "socket.io";
+import app from "./app";
+import { logger } from "./utils/logger";
+import AuthController from "./controllers/authController";
 
 const server = http.createServer(app);
-// const io = new Server(server);
+const io = new Server(server);
 
-// io.on('connection', (socket) => {
-//     logger.info('New WebSocket connection');
 
-//     socket.on('joinRoom', (room) => {
-//         socket.join(room);
-//         logger.info(`User joined room: ${room}`);
-//     });
+io.on("connection", (socket) => {
+  logger.info("New WebSocket connection");
 
-//     socket.on('chatMessage', (msg) => {
-//         io.to(msg.room).emit('message', msg);
-//         logger.info(`Message: ${msg.text} sent to room: ${msg.room}`);
-//     });
+  socket.on("joinRoom", (room) => {
+    try {
+      AuthController.checkAuth(socket.handshake.headers.tk as string);
+    } catch (error: any) {
+      logger.error(`Authintication Error: ${error.message}`);
+      return;
+    }
+    socket.join(room);
+    logger.info(`User joined room: ${room}`);
+  });
 
-//     socket.on('disconnect', () => {
-//         logger.info('User disconnected');
-//     });
-// });
+  socket.on("chatMessage", (msg) => {
+    try {
+      AuthController.checkAuth(socket.handshake.headers.tk as string);
+    } catch (error: any) {
+      logger.error(`Authintication Error: ${error.message}`);
+      return;
+    }
+    io.to(msg.room).emit("message", msg);
+    logger.info(`Message: ${msg.text} sent to room: ${msg.room}`);
+  });
+
+  socket.on("leaveRoom", (room) => {
+    try {
+      AuthController.checkAuth(socket.handshake.headers.tk as string);
+    } catch (error: any) {
+      logger.error(`Authintication Error: ${error.message}`);
+      return;
+    }
+    socket.leave(room);
+    logger.info(`User left room: ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    logger.info("User disconnected");
+  });
+
+  socket.on("error", (error) => {
+    logger.error(`Socket Error: ${error.message}`);
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
