316d3f59a1367d7941103956567b4fba08bf801d
Rahim Daud <rahimdaud246@gmail.com>
2024-07-18 02:10:56 +0000

    chore: Update logger configuration and file paths

--- api/utils/logger.ts
+++ api/utils/logger.ts
@@ -1,36 +1,31 @@
-import { createLogger, format, transports } from "winston"
+import { createLogger, format, transports } from "winston";
 
 const consoleFormat = format.combine(
     format.colorize(),
     format.timestamp(),
     format.align(),
     format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-)
+);
 
 const fileFormat = format.combine(
     format.timestamp(),
     format.errors({ stack: true }),
     format.splat(),
     format.json()
-)
+);
+
+const loggerTransports = [
+    new transports.Console({
+        format: consoleFormat
+    })
+];
+
 
 const logger = createLogger({
     level: "info",
     format: fileFormat,
     defaultMeta: { service: "user-service" },
-    transports: [
-        new transports.File({ filename: "logs/error.log", level: "error" }),
-        new transports.File({ filename: "logs/combined.log" })
-    ]
-}
-)
+    transports: loggerTransports
+});
 
-if (process.env.NODE_ENV !== "production") {
-    logger.add(new transports.Console({
-        format: consoleFormat
-    }))
-}
-
-
-
-export { logger }
+export { logger };
